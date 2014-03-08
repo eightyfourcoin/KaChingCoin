dca862ee06a81751a8805f46b92591d7745b31ee
Brandon Bechtel <brandon@Ubuntu-Ka-ChingCoin.(none)>
2014-03-07 20:04:30 -0500

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("KAC");
+    case mBTC: return QString("mKAC");
+    case uBTC: return QString::fromUtf8("μKAC");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("KaChingCoin");
+    case mBTC: return QString("Milli-KaChingCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-KaChingCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
